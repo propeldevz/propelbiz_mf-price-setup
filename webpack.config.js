3310418954f68cb100ac9890fc37787a30c1877a
MrAkashSoni <akashpie3@gmail.com>
2025-10-15 23:31:15 +0530

    added_allowedhost

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,17 @@ module.exports = {
   },
   devServer: {
     port: 3011,
+    host: "0.0.0.0",
     historyApiFallback: true,
+    allowedHosts: "all",
     static: {
       directory: path.join(__dirname, "public"),
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
     },
   },
   module: {
